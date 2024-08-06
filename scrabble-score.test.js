f7eecb1ef1f16d28cf1167ad2b09b62e8ec50038
dtron500 <daniel.mitchell3005@gmail.com>
2024-08-06 16:54:25 +0100

    Added letters with score

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,5 +1,17 @@
-import { calculateScrabbleScore } from "./scrabble-score";
+import { calculateScrabbleScore } from "./scrabble-score.js";
+
+import {test, expect} from "vitest"
 
 test("Testing points are correctly assigned", () => {
     expect(calculateScrabbleScore("A")).toBe(1)
 });
+
+test("Testing that Q = 10", () =>{
+    expect(calculateScrabbleScore("Q")).toBe(10)
+}
+)
+
+
+test("Testing that cat = 5 points", () => {
+    expect(calculateScrabbleScore())
+})
